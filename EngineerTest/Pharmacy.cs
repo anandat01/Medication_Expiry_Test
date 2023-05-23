9220ca90d257b5833bd1b46fbca37968658b23c7
Ananda Tiwari <anandatiwari31@gmail.com>
2023-05-23 23:34:34 +0930

    Refactored UpdateBenefitValue function; added functionality for new drug added

--- EngineerTest/Pharmacy.cs
+++ EngineerTest/Pharmacy.cs
@@ -11,82 +11,83 @@ public class Pharmacy : IPharmacy
 
     public IEnumerable<IDrug> UpdateBenefitValue()
     {
+        //defining maximum and minimum benefits value
+        int MaxBenefit = 50;
+        int MinBenefit = 0;
+
+        //looping through all drug to implement specific conditions
         for (var i = 0; i < _drugs.Length; i++)
         {
-            if (
-                _drugs[i].Name != "Herbal Tea" &&
-                _drugs[i].Name != "Fervex"
-            )
+            switch (_drugs[i].Name)
             {
-                if (_drugs[i].Benefit > 0)
-                {
-                    if (_drugs[i].Name != "Magic Pill")
-                    {
-                        _drugs[i].Benefit = _drugs[i].Benefit - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (_drugs[i].Benefit < 50)
-                {
-                    _drugs[i].Benefit = _drugs[i].Benefit + 1;
-                    if (_drugs[i].Name == "Fervex")
-                    {
-                        if (_drugs[i].ExpiresIn < 11)
-                        {
-                            if (_drugs[i].Benefit < 50)
-                            {
-                                _drugs[i].Benefit = _drugs[i].Benefit + 1;
-                            }
-                        }
+                case "Magic Pill":
+                    _drugs[i].Benefit = _drugs[i].Benefit;
+                    _drugs[i].ExpiresIn = _drugs[i].ExpiresIn;
+                    break;
 
-                        if (_drugs[i].ExpiresIn < 6)
-                        {
-                            if (_drugs[i].Benefit < 50)
-                            {
-                                _drugs[i].Benefit = _drugs[i].Benefit + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (_drugs[i].Name != "Magic Pill")
-            {
-                _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
-            }
-
-            if (_drugs[i].ExpiresIn < 0)
-            {
-                if (_drugs[i].Name != "Herbal Tea")
-                {
-                    if (_drugs[i].Name != "Fervex")
+                case "Herbal Tea":
+                    if (_drugs[i].ExpiresIn > 0)
                     {
-                        if (_drugs[i].Benefit > 0)
-                        {
-                            if (_drugs[i].Name != "Magic Pill")
-                            {
-                                _drugs[i].Benefit = _drugs[i].Benefit - 1;
-                            }
-                        }
+                        _drugs[i].Benefit = (_drugs[i].Benefit + 1) <= MaxBenefit ? (_drugs[i].Benefit + 1) : MaxBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
                     }
                     else
                     {
-                        _drugs[i].Benefit =
-                            _drugs[i].Benefit - _drugs[i].Benefit;
+                        _drugs[i].Benefit = (_drugs[i].Benefit + 2) <= MaxBenefit ? (_drugs[i].Benefit + 2) : MaxBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
                     }
-                }
-                else
-                {
-                    if (_drugs[i].Benefit < 50)
+                    break;
+
+                case "Fervex":
+                    if (_drugs[i].ExpiresIn > 10)
                     {
-                        _drugs[i].Benefit = _drugs[i].Benefit + 1;
+                        _drugs[i].Benefit = (_drugs[i].Benefit + 1) <= MaxBenefit ? (_drugs[i].Benefit + 1) : MaxBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
                     }
-                }
+                    else if (_drugs[i].ExpiresIn <= 10 && _drugs[i].ExpiresIn > 5)
+                    {
+                        _drugs[i].Benefit = (_drugs[i].Benefit + 2) <= MaxBenefit ? (_drugs[i].Benefit + 2) : MaxBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    else if (_drugs[i].ExpiresIn <= 5 && _drugs[i].ExpiresIn > 0)
+                    {
+                        _drugs[i].Benefit = (_drugs[i].Benefit + 3) <= MaxBenefit ? (_drugs[i].Benefit + 3) : MaxBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    else
+                    {
+                        _drugs[i].Benefit = 0;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    break;
+
+                case "Dafalgan":
+                    if (_drugs[i].ExpiresIn > 0)
+                    {
+                        _drugs[i].Benefit = (_drugs[i].Benefit - 2) > MinBenefit ? (_drugs[i].Benefit - 2) : MinBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    else
+                    {
+                        _drugs[i].Benefit = (_drugs[i].Benefit - 4) > MinBenefit ? (_drugs[i].Benefit - 4) : MinBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    break;
+
+                default:
+                    if (_drugs[i].ExpiresIn > 0)
+                    {
+                        _drugs[i].Benefit = (_drugs[i].Benefit - 1) > MinBenefit ? (_drugs[i].Benefit - 1) : MinBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    else
+                    {
+                        _drugs[i].Benefit = (_drugs[i].Benefit - 2) > MinBenefit ? (_drugs[i].Benefit - 2) : MinBenefit;
+                        _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                    }
+                    break;
             }
         }
-
         return _drugs;
     }
 }
