55d511b8c57ab9dfb96e3a790bb2104404576624
Ananda Tiwari <anandatiwari31@gmail.com>
2023-05-23 23:42:13 +0930

    Changed Output file to compare the o/p with expected given o/p

--- EngineerTest/Program.cs
+++ EngineerTest/Program.cs
@@ -5,6 +5,7 @@ using EngineerTest;
 
 try
 {
+    //list of drug (string Name, int ExpiresIn, int Benefit)
     var drugs = new[] {
         new Drug("Doliprane", 20, 30),
         new Drug("Herbal Tea", 10, 5),
@@ -20,11 +21,12 @@ try
         log.Add(JsonSerializer.Deserialize<Drug[]>(JsonSerializer.Serialize(pharmacy.UpdateBenefitValue())));
     }
 
+    //writing output in file: new_output.json
     File.WriteAllText(
-        "output.json",
-        JsonSerializer.Serialize(new { Result= log }, options: new JsonSerializerOptions { WriteIndented = true})
-    );    
-    Console.WriteLine("Success");
+        "new_output.json",
+        JsonSerializer.Serialize(new { Result = log }, options: new JsonSerializerOptions { WriteIndented = true })
+    );
+    Console.WriteLine("Success, please check new_output.Json file for generated output");
 
 }
 catch
