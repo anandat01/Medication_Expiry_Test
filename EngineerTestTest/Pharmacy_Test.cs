a319771005adfbb5a2133acedb8bb28c76883276
Ananda Tiwari <anandatiwari31@gmail.com>
2023-05-23 23:48:33 +0930

    tested component

---
+++ EngineerTestTest/Pharmacy_Test.cs
@@ -0,0 +1,14 @@
+﻿using EngineerTest;
+using Xunit;
+
+namespace EngineerTestTest;
+
+public class TestPharmacy
+{
+    [Fact]
+    public void Test_DrugBenefitExpiryValue()
+    {
+        Assert.Equal(new Drug[] { new Drug("paracetamole", 10, 20) },
+            new Pharmacy(new Drug[] { new Drug("paracetamole", 11, 21) }).UpdateBenefitValue());
+    }
+}
